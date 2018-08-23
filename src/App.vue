7aa5833efb6f2e4fa7c5cb81c970720bc9789765
Source <598481331@qq.com>
2018-08-23 13:45:02 +0800

    play

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
 <template>
-    <router-view></router-view>
-</template>
+<!-- <router-view></router-view> -->
+<div>
+  <input v-model="value" v-on:keyup.enter="addNew">
+  <button @click="delete1">delete</button>
+    <h1 v-for="item in items">
+      <ul>
+        <li v-text="item.label"  v-on:click='finish(item)' v-bind:class="{Finished:item.isFinished}"></li>
+      </ul>
+    </h1>
+</div>
+</template> 
+
+<script>
+export default{
+  data(){
+    return{
+      items:[],
+      value:''
+    }
+  },
+  methods:{
+    addNew : function(){
+      this.items.push({label:this.value,isFinished:false})
+    },
+    delete1 : function(){
+      this.items.pop()
+    },
+    finish : function(item){
+      item.isFinished=!item.isFinished
+    }
+  }
+}
+</script>
 
   <style>
-    
+    .Finished{
+      font-size: 90px
+    }
   </style>
