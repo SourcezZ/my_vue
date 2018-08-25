8b7b53cb115492b4b229529636724878b139ec6f
Source <598481331@qq.com>
2018-08-25 14:37:35 +0800

    local storage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,57 @@
 <template>
-    <router-view></router-view>
+  <!-- <router-view></router-view> -->
+  <div>
+    <el-input class="input"v-model="value" v-on:keyup.enter.native="addNew"></el-input>
+    <el-button @click="delete1">delete</el-button>
+    <el-button @click="this.text1">global</el-button>
+    <h1 v-for="item in items">
+      <ul>
+        <li v-text="item.label" v-on:click='finish(item)' v-bind:class="{Finished:item.isFinished}"></li>
+      </ul>
+    </h1>
+  </div>
 </template>
 
-  <style>
-  </style>
+<script>
+  import Store from './store'
+  export default {
+    data() {
+      return {
+        items: Store.fetch(),
+        value: ''
+      }
+    },
+    watch:{
+        items:{
+          handler:function(items){
+            Store.save(items)
+          },
+          deep:true
+        }
+      },
+    methods: {
+      addNew: function () {
+        this.items.push({ 
+          label: this.value, 
+          isFinished: false 
+          })
+          this.value=''
+      },
+      delete1: function () {
+        this.items.pop()
+      },
+      finish: function (item) {
+        item.isFinished = !item.isFinished
+      },
+    }
+  }
+</script>
+
+<style>
+  .Finished {
+    text-decoration: underline
+  }
+  .input{
+    width: 200px
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
-import func from './function/index'
+import func from './function/func'
 
 Vue.config.productionTip = false
 
